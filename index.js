34693048fdf0a2d3cc98f4a063d43b1a2a5e344b
tchappel <tchappel@ebay.com>
2020-02-25 16:47:37 +0100

    refactor with class and change icons

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
-const nav = document.querySelector(".nav");
-const navCheckbox = nav.querySelector(".nav__checkbox");
-const navToggleBtn = nav.querySelector(".nav__toggle-btn");
-const navLinks = nav.querySelector(".nav__links");
-let navTransitionTimeout = null;
-const navTransitionTime = 300; // this must correspond to transition time in class "nav-transition"
-const navBreakpoint = 600; // this should correspond to mediaQuery for navbar in css
-
-const isNavCheckboxChecked = () => {
-  if (!navCheckbox) return false;
-  return navCheckbox.checked;
-};
-
-const addTransitionToNavLinks = () => {
-  navLinks.classList.add("nav-transition");
-  clearTimeout(navTransitionTimeout);
-  navTransitionTimeout = setTimeout(() => {
-    navLinks.classList.remove("nav-transition");
-  }, navTransitionTime);
-};
-
-const openNavLinks = () => {
-  if (!isNavCheckboxChecked) navCheckbox.checked = true;
-  nav.classList.add("open");
-};
-
-const closeNavLinks = () => {
-  if (isNavCheckboxChecked) navCheckbox.checked = false;
-  nav.classList.remove("open");
-};
-
-window.addEventListener("click", () => {
-  if (isNavCheckboxChecked()) {
-    openNavLinks();
-    addTransitionToNavLinks();
-  } else {
-    closeNavLinks();
-    addTransitionToNavLinks();
+class Nav {
+  constructor({navId, navTransitionTime, navBreakpoint}) {
+    this.nav = document.querySelector(`.nav#${navId}`);
+    this.navCheckbox = this.nav.querySelector(".nav__checkbox");
+    this.navToggleBtn = this.nav.querySelector(".nav__toggle-btn");
+    this.navLinks = this.nav.querySelector(".nav__links");
+    this.navTransitionTimeout = null;
+    this.navTransitionTime = navTransitionTime; // this must correspond to transition time in class "nav-transition"
+    this.navBreakpoint = navBreakpoint; // this should correspond to mediaQuery for navbar in css
+    this.init();
   }
-});
 
-window.addEventListener("resize", e => {
-  const clientWidth = document.body.clientWidth;
-  if (clientWidth > navBreakpoint) {
-    closeNavLinks();
+  shouldOpenNavbar = () => {
+    if (!this.navCheckbox) return false;
+    return this.navCheckbox.checked;
+  };
+
+  addTransitionToNavLinks = () => {
+    this.navLinks.classList.add("nav-transition");
+    clearTimeout(this.navTransitionTimeout);
+    this.navTransitionTimeout = setTimeout(() => {
+      this.navLinks.classList.remove("nav-transition");
+    }, this.navTransitionTime);
+  };
+
+  openNav = () => {
+    if (!this.shouldOpenNavbar()) this.navCheckbox.checked = true;
+    this.nav.classList.add("open");
+  };
+
+  closeNav = () => {
+    if (this.shouldOpenNavbar()) this.navCheckbox.checked = false;
+    this.nav.classList.remove("open");
+  };
+
+  init = () => {
+    window.addEventListener("click", () => {
+      if (this.shouldOpenNavbar()) {
+        this.openNav();
+        this.addTransitionToNavLinks();
+      } else {
+        this.closeNav();
+        this.addTransitionToNavLinks();
+      }
+    });
+
+    window.addEventListener("resize", e => {
+      const clientWidth = document.body.clientWidth;
+      if (clientWidth > this.navBreakpoint) {
+        this.closeNav();
+      }
+    });
   }
-});
+}
+
+const nav1 = new Nav({
+  navId: 'nav-main',
+  navTransitionTime: 300,
+  navBreakpoint: 600
+})
