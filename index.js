484b35a08d49d1ba81abeebd03bc56e410c518b7
tchappel <tchappel@ebay.com>
2020-02-25 15:12:44 +0100

    refactor

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-const nav = document.querySelector('.nav')
-const navCheckbox = nav.querySelector('.nav__checkbox')
-const navLinks = nav.querySelector('.nav__links')
-let navTransitionTimeout = null
+const nav = document.querySelector(".nav");
+const navCheckbox = nav.querySelector(".nav__checkbox");
+const navToggleBtn = nav.querySelector(".nav__toggle-btn");
+const navLinks = nav.querySelector(".nav__links");
+let navTransitionTimeout = null;
 const navTransitionTime = 300; // this must correspond to transition time in class "nav-transition"
 const navBreakpoint = 600; // this should correspond to mediaQuery for navbar in css
 
 const isNavCheckboxChecked = () => {
-    if (!navCheckbox) return false
-    return navCheckbox.checked
-}
+  if (!navCheckbox) return false;
+  return navCheckbox.checked;
+};
 
 const addTransitionToNavLinks = () => {
-    navLinks.classList.add('nav-transition')
-    clearTimeout(navTransitionTimeout)
-    navTransitionTimeout = setTimeout(() => {
-        navLinks.classList.remove('nav-transition')
-    }, navTransitionTime)
-}
+  navLinks.classList.add("nav-transition");
+  clearTimeout(navTransitionTimeout);
+  navTransitionTimeout = setTimeout(() => {
+    navLinks.classList.remove("nav-transition");
+  }, navTransitionTime);
+};
 
 const openNavLinks = () => {
-    if (!isNavCheckboxChecked) navCheckbox.checked = true
-    navLinks.classList.add('open')
-}
+  if (!isNavCheckboxChecked) navCheckbox.checked = true;
+  nav.classList.add("open");
+};
 
 const closeNavLinks = () => {
-    if (isNavCheckboxChecked) navCheckbox.checked = false
-    navLinks.classList.remove('open')
-}
+  if (isNavCheckboxChecked) navCheckbox.checked = false;
+  nav.classList.remove("open");
+};
 
-navCheckbox.addEventListener('click', () => {
-    if(isNavCheckboxChecked()) {
-        openNavLinks()
-        addTransitionToNavLinks()
-    } else {
-        closeNavLinks()
-        addTransitionToNavLinks()
-    }
-})
+window.addEventListener("click", () => {
+  if (isNavCheckboxChecked()) {
+    openNavLinks();
+    addTransitionToNavLinks();
+  } else {
+    closeNavLinks();
+    addTransitionToNavLinks();
+  }
+});
 
 window.addEventListener("resize", e => {
-    const clientWidth = document.body.clientWidth;
-    if (clientWidth > navBreakpoint) {
-        closeNavLinks()
-    }
+  const clientWidth = document.body.clientWidth;
+  if (clientWidth > navBreakpoint) {
+    closeNavLinks();
+  }
 });
